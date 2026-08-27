f075daa2d36e2396d19b71f774ad2f672d4dba30
Alize <alizewulf@gmail.com>
2026-08-27 21:02:14 +0400

    fix: sidebar abstracticon colors fix

--- frontend/src/widgets/Layout/AppLayout/ui/Sidebar/Sidebar.Columns.tsx
+++ frontend/src/widgets/Layout/AppLayout/ui/Sidebar/Sidebar.Columns.tsx
@@ -1,13 +1,14 @@
 import useColumns from "../../../../../features/columns/useColumns/useColumns"
 import textStyles from "../../../../../shared/typography/typography";
-import AbstractIcon from "./icons/AbstractIcon";
+import AbstractIcon, { iconFillColors } from "./icons/AbstractIcon";
 import { useAppContext } from "../../../../../shared/context/app.context";
+import { useState } from "react";
 
 function SidebarColumns({ userId }: { userId: number }) {
 
   const { data, loading, error } = useColumns(userId)
   const { setSelectedColumn } = useAppContext();
-
+  const [activeColumn, setActiveColumn] = useState<number>(1)
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -26,10 +27,13 @@ function SidebarColumns({ userId }: { userId: number }) {
         {data.map((column) => (
           <li
             key={column.id}
-            className={`${textStyles.heading.md} cursor-pointer py-4 pl-8 font-bold capitalize flex gap-4 items-center`}
-            onClick={() => setSelectedColumn(column)}
+            className={`${textStyles.heading.md} cursor-pointer py-4 pl-8 font-bold capitalize flex gap-4 items-center ${activeColumn === column.id? "text-white bg-primary rounded-r-full" : "text-accent3-hover"}`}
+            onClick={() => { 
+              setSelectedColumn(column) 
+              setActiveColumn(column.id)
+            }}
           >
-            <AbstractIcon />
+            <AbstractIcon fill={activeColumn === column.id? iconFillColors.active :  iconFillColors.inactive}/>
             {column.title}
           </li>
         ))}
@@ -37,7 +41,7 @@ function SidebarColumns({ userId }: { userId: number }) {
         <li
           className="text-primary text-[15px] py-4 pl-8 capitalize cursor-pointer flex gap-4 font-bold items-center"
         >
-          <AbstractIcon />
+          <AbstractIcon fill={iconFillColors.create} />
           + Create New Board
         </li>
       </ul>
